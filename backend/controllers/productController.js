3b46692c03bae8783e70a6ebe5b721b0873359c0
Jakub <jakub2002t@gmail.com>
2025-04-11 21:24:21 +0200

    fixing backend v3

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,15 +5,24 @@ exports.getAllProducts = async (req, res) => {
     const products = await Product.getAll();
     res.status(200).json(products);
   } catch (error) {
-    res.status(500).json({ message: 'Błąd podczas pobierania produktów', error: error.message });
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getProductById = async (req, res) => {
+  try {
+    const product = await Product.getById(req.params.id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
 
 exports.createProduct = async (req, res) => {
   try {
     const productId = await Product.create(req.body);
-    res.status(201).json({ message: 'Produkt został utworzony', id: productId });
+    res.status(201).json({ id: productId });
   } catch (error) {
-    res.status(500).json({ message: 'Błąd podczas tworzenia produktu', error: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
